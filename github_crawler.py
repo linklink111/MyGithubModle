14e547c5ceef7ac92e11c6cd25a5265222eb02a1
787624675 <787624675@qq.com>
2021-09-13 18:29:25 +0800

    数据库未连接成功

--- github_crawler.py
+++ github_crawler.py
@@ -7,15 +7,24 @@ import os
 
 # 为了防止本人的数据库密码被传到 github，采用了配置文件= =
 cur_dir_path = os.path.abspath(".")
-secret_yaml_file_path = os.path.join(cur_dir_path, "secret")
+secret_yaml_file_path = os.path.join(cur_dir_path, "secret.yaml")
 secret_yaml_file = open(secret_yaml_file_path,'r',encoding='utf8')
-secret_data = secret_yaml_file.read()
+secret_fdata = secret_yaml_file.read()
 secret_yaml_file.close()
+secret_data = yaml.load(secret_fdata)
+print(secret_data)
 
+sql_yaml_file_path = os.path.join(cur_dir_path, "sql.yaml")
+sql_yaml_file = open(sql_yaml_file_path,'r',encoding='utf8') 
+sql_fdata = sql_yaml_file.read()
+sql_yaml_file.close()
+sql_data = yaml.load(sql_fdata)
+print(sql_data)
 # 连接数据库
-secret_data = yaml.load()
+
 db_name = "giteehub"
 user = "postgres"
+print(secret_data['password'])
 mpassword = secret_data['password']
 db_conn = psycopg2.connect(
     database = "giteehub", 
@@ -23,3 +32,16 @@ db_conn = psycopg2.connect(
     password = mpassword, 
     host = "127.0.0.1", 
     port = "1062")
+
+# 用游标执行数据库操作
+
+cursor = db_conn.cursor()
+# 测试一下有没有连通数据库
+
+try:
+    cursor.execute(sql_data['testInsert'])
+    db_conn.commit()
+    db_conn.close()
+except Exception:
+    print(Exception)
+
